c575308e8208351078218c23f7314ce761ec6f61
Lewingston <jonas.speyer@web.de>
2026-04-18 15:04:48 +0200

    Fixed orthographic projection

--- src/renderer/camera.rs
+++ src/renderer/camera.rs
@@ -35,7 +35,7 @@ pub struct Camera {
 const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::from_cols(
     cgmath::Vector4::new(1.0, 0.0, 0.0, 0.0),
     cgmath::Vector4::new(0.0, 1.0, 0.0, 0.0),
-    cgmath::Vector4::new(0.0, 0.0, 0.5, 1.0),
+    cgmath::Vector4::new(0.0, 0.0, 0.5, 0.0),
     cgmath::Vector4::new(0.0, 0.0, 0.5, 1.0)
 );
 
