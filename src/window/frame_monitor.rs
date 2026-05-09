3b389173ae71d5238f93f9e68134607a19a13f55
Lewingston <jonas.speyer@web.de>
2026-05-09 16:46:19 +0200

    Use different implementation for time library when compiling for wasm32.

--- src/window/frame_monitor.rs
+++ src/window/frame_monitor.rs
@@ -1,10 +1,16 @@
 
 use std::collections::VecDeque;
 
+#[cfg(target_arch = "wasm32")]
+use web_time::{Instant, Duration};
+
+#[cfg(not(target_arch = "wasm32"))]
+use std::time::{Instant, Duration};
+
 
 pub struct FrameMonitor {
 
-    start_time:          std::time::Instant,
+    start_time:          Instant,
     timing_queue:        VecDeque<std::time::Duration>,
     frame_average_count: usize
 }
@@ -15,7 +21,7 @@ impl FrameMonitor {
     pub fn new() -> Self {
 
         Self {
-            start_time:          std::time::Instant::now(),
+            start_time:          Instant::now(),
             timing_queue:        VecDeque::<std::time::Duration>::new(),
             frame_average_count: 30
         }
@@ -24,7 +30,7 @@ impl FrameMonitor {
 
     pub fn start_frame(&mut self) {
 
-        self.start_time = std::time::Instant::now();
+        self.start_time = Instant::now();
     }
 
 
@@ -41,9 +47,9 @@ impl FrameMonitor {
     }
 
 
-    pub fn get_frame_time(&self) -> std::time::Duration {
+    pub fn get_frame_time(&self) -> Duration {
 
-        let mut total_duration = std::time::Duration::new(0, 0);
+        let mut total_duration = Duration::new(0, 0);
 
         for duration in &self.timing_queue {
 
@@ -51,7 +57,7 @@ impl FrameMonitor {
         }
 
         if self.timing_queue.len() == 0 {
-            std::time::Duration::new(0, 0)
+            Duration::new(0, 0)
         } else {
             total_duration / self.timing_queue.len() as u32
         }
